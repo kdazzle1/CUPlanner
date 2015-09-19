f2ff5e3d0fcc0026dbf07461a1c1e8a21cfaf61f
Kevin <kevinshen8@optonline.net>
2015-09-18 20:21:40 -0400

    elo hello

--- app/src/main/java/com/kdazzle/myapplication/MainActivity.java
+++ app/src/main/java/com/kdazzle/myapplication/MainActivity.java
@@ -4,8 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onClick (View v)
+    {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
